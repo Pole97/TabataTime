a1ccbac40f7a44c0791701a2836482e868fcb61d
Pole97 <32852461+Pole97@users.noreply.github.com>
2025-04-22 22:46:29 +0200

    🔧 changed ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 android {
     namespace = "com.polebyte.tabata_time"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
